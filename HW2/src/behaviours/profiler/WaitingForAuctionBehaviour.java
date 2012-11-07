9838d5dc9c529e7cbb136e8eb264596757e7c613
Anders Lehndal <pinballmilitia@gmail.com>
2012-11-07 12:38:38 +0100

    added profiler behaviours to handle auction

---
+++ HW2/src/behaviours/profiler/WaitingForAuctionBehaviour.java
@@ -0,0 +1,38 @@
+//WaitingForAuctionBehaviour.java
+/************************************************************************
+ * This class represents the behaviour of the profileragent as he is 
+ * waiting for an auction.
+ ************************************************************************/
+
+package behaviours.profiler;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.FIPANames;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class WaitingForAuctionBehaviour extends CyclicBehaviour {
+
+	private ACLMessage msg;
+	private String highest_price;
+	
+	@Override
+	public void action() {
+		
+		//message templte for dutch auction
+		MessageTemplate mt = MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_DUTCH_AUCTION);
+
+		msg = myAgent.receive(mt);
+		
+		if(msg != null) {
+			if(msg.getPerformative() == ACLMessage.INFORM) {
+				if(msg.getOntology().equals("AUCTION")) {		
+					System.out.println(myAgent.getLocalName() + ": auction has started");
+					
+					//Auction with 100 as this agents highest buying price
+					myAgent.addBehaviour(new NewAuctionBehaviour("100"));
+				}
+			}	
+		}
+	}
+}
