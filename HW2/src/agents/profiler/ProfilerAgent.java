8cb8840a16adc1612b1288db8f8df3b411705b57
Anders Lehndal <pinballmilitia@gmail.com>
2012-11-07 12:42:20 +0100

    updated profileragent

--- HW2/src/agents/profiler/ProfilerAgent.java
+++ HW2/src/agents/profiler/ProfilerAgent.java
@@ -10,17 +10,53 @@
 
 package agents.profiler;
 
+import behaviours.profiler.WaitingForAuctionBehaviour;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.AMSService;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 @SuppressWarnings("serial")
 public class ProfilerAgent extends Agent {
 	
-	protected void setup() {		
-		System.out.println(getLocalName() + ": starting.");		
+	protected void setup() {	
+		System.out.println(getLocalName() + ": starting.");			
 
+		//Register agent at DF as buyer
+		registerAsBuyer();
+		
+		//Wait for start of auction
+		addBehaviour(new WaitingForAuctionBehaviour());	
 	}
 	
 	protected void takeDown() { 
 		System.out.println(getLocalName() + ": goodbye.");
+	}
+	
+	private void registerAsBuyer() {
+		
+		//Register agent at DF as 'buyer'
+		DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName( getAID() ); 
+        ServiceDescription sd  = new ServiceDescription();
+        sd.addProtocols(FIPANames.InteractionProtocol.FIPA_DUTCH_AUCTION);	
+        sd.setType("buyer");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        
+        try {  
+        	System.out.println(getLocalName() + ": registering himself at DF");
+            DFService.register(this, dfd );  
+        }
+        catch (FIPAException fe) { fe.printStackTrace(); }
+           
 	}
 }
